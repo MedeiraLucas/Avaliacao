166b9af23ed8b2457764962f0df702b0b87907d2
Lucas Medeira <lucasdracula@gmail.com>
2024-11-05 20:38:35 -0300

    sql

--- Cadastro.java
+++ Cadastro.java
@@ -1,3 +1,13 @@
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
